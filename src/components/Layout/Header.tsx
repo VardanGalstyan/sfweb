3494d3ff2ea1f6d5720f98f478d60491b3b6c671
Vardan Galstyan <vardan.galstjan@gmail.com>
2023-02-11 11:31:05 +0100

    fix: getting ready for initial deployment

--- src/components/Layout/Header.tsx
+++ src/components/Layout/Header.tsx
@@ -37,18 +37,18 @@ export default function Header() {
                 <div className="hidden sm:ml-6 sm:block">
                   <div className="flex space-x-4">
                     {navigation.map((item) => (
-                      <Link key={item.name} href={item.href}>
-                        <a
-                          className={classNames(
-                            item.current
-                              ? "bg-gray-900 text-white"
-                              : "text-gray-300 hover:bg-gray-700 hover:text-white",
-                            "rounded-md px-3 py-2 text-sm font-medium"
-                          )}
-                          aria-current={item.current ?? item.href}
-                        >
-                          {item.name}
-                        </a>
+                      <Link
+                        key={item.name}
+                        href={item.href}
+                        className={classNames(
+                          item.current
+                            ? "bg-gray-900 text-white"
+                            : "text-gray-300 hover:bg-gray-700 hover:text-white",
+                          "rounded-md px-3 py-2 text-sm font-medium"
+                        )}
+                        aria-current={item.current ?? item.href}
+                      >
+                        {item.name}
                       </Link>
                     ))}
                   </div>
